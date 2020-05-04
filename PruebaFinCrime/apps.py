857a035fff0d039a33c61a8018f6171d513282d3
Mariano Roque Masi <Mariano.Roque.Masi@es.ey.com>
2020-05-04 15:38:35 +0200

    Initial commit

---
+++ PruebaFinCrime/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PruebafincrimeConfig(AppConfig):
+    name = 'PruebaFinCrime'
---
+++ PruebaFinCrime/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def PruebaFinCrime(request):
+    return render(request,'PruebaFinCrime.html',{})
